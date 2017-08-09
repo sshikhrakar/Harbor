696760352be070e72a72afb587b0ae9b9dcad314
srishanbhattarai <srishanbhattarai@gmail.com>
2017-08-09 21:05:42 +0545

    (refactor) Remove Add project view

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,7 @@
 import HomePage from './HomePage';
 import LandingPage from './LandingPage';
-import AddProjectPage from './AddProjectPage';
 
 export {
   HomePage,
   LandingPage,
-  AddProjectPage,
 };
