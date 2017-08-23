47020f950c1ff0fc83d6436f245498fc7cb3105b
srishanbhattarai <srishanbhattarai@gmail.com>
2017-08-23 17:35:25 +0545

    (del) Remove firebase config

--- src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAD8YUdaxfGRe4PtAnWJivxht52IlZEbgg',
-  authDomain: 'harbor-c9a5f.firebaseapp.com',
-  databaseURL: 'https://harbor-c9a5f.firebaseio.com/',
-  storageBucket: 'harbor-c9a5f.appspot.com/',
-  messagingSenderId: '123456789',
-};
-
-export default firebaseConfig;
