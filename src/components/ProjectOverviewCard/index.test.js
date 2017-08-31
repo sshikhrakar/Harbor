2dee83019b48c3cd07edd063dadc2c17da05335f
srishanbhattarai <srishanbhattarai@gmail.com>
2017-08-31 15:29:18 +0545

    (feat) Add project card component

---
+++ src/components/ProjectOverviewCard/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import ProjectOverviewCard from './index';
+
+describe('COMPONENTS: ProjectOverviewCard', () => {
+
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <ProjectOverviewCard
+        onCardPress={ () => true }
+        displayName={ 'asdf' }
+        versionNumber={ 'v1.1.0' }
+        lastUpdatedAt={ 'Wed, Aug30, 2017' }
+        projectIconUrl={ 'http://asdfadsf.com'}
+      />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+});
