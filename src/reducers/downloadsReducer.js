961c12850570cef905647d646de83e2cb5d13c3e
Srishan <srishanbhattarai@gmail.com>
2017-09-08 08:44:46 +0545

    (wip) Move apk downloaded bool and apk path data to downloads reducer, so that it doesnt get overriden after api calls.

--- src/reducers/downloadsReducer.js
+++ src/reducers/downloadsReducer.js
@@ -8,6 +8,7 @@ import {
 const initialState = {
   isDownloading: false,
   currentDownload: null,
+  projects: {},
 };
 
 /**
@@ -28,6 +29,7 @@ function downloadsReducer(state = initialState, action) {
           ...action.payload.project,
           progress: 0,
         },
+        projects: projectsReducer(state.projects, action),
       };
 
     case UPDATE_CURRENT_DOWNLOAD_PROGRESS:
@@ -47,12 +49,103 @@ function downloadsReducer(state = initialState, action) {
           ...state.currentDownload,
           progress: 100,
         },
+        projects: projectsReducer(state.projects, action),
       };
 
     case DOWNLOAD_ERRORED:
       return {
         ...state,
         isDownloading: false,
+        projects: projectsReducer(state.projects, action),
+      };
+
+    default:
+      return state;
+  }
+}
+
+/**
+ * Reducer for state.downloads.projects slice.
+ * Not to be mixed with state.projects reducer.
+ *
+ * @param {Object} state
+ * @param {Object} action
+ * @returns {Object}
+ */
+function projectsReducer(state, action) {
+  switch(action.type) {
+    case DOWNLOAD_COMPLETED:
+      return {
+        ...state,
+        [action.payload.project.packageName]: projectReducer(state[action.payload.project.packageName], action),
+      };
+
+    case DOWNLOAD_STARTED:
+      return {
+        ...state,
+        [action.payload.project.packageName]: projectReducer(state[action.payload.project.packageName], action),
+      };
+
+    case DOWNLOAD_ERRORED:
+      return {
+        ...state,
+        [action.payload.project.packageName]: projectReducer(state[action.payload.project.packageName], action),
+      };
+
+    default:
+      return state;
+  }
+}
+
+
+/**
+ * Reducer for a single project.
+ *
+ * @param {Object} state
+ * @param {Object} action
+ * @returns {Object}
+ */
+function projectReducer(state, action) {
+  switch(action.type) {
+
+    case DOWNLOAD_STARTED:
+      return {
+        ...state,
+        uploads: {
+          ...state.uploads,
+          [action.payload.timestamp]: {
+            ...state.uploads[action.payload.timestamp],
+            downloading: true,
+          },
+        },
+      };
+
+
+    case DOWNLOAD_COMPLETED:
+      return {
+        ...state,
+        uploads: {
+          ...state.uploads,
+          [action.payload.timestamp]: {
+            ...state.uploads[action.payload.timestamp],
+            downloading: false,
+            downloaded: true,
+            apkPath: action.payload.apkPath,
+          },
+        },
+      };
+
+    case DOWNLOAD_ERRORED:
+      return {
+        ...state,
+        uploads: {
+          ...state.uploads,
+          [action.payload.timestamp]: {
+            ...state.uploads[action.payload.timestamp],
+            downloading: false,
+            downloaded: false,
+          },
+        },
       };
 
     default:
--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -1,6 +1,4 @@
 import {
-  DOWNLOAD_STARTED,
-  DOWNLOAD_COMPLETED,
   FETCH_ALL_PROJECTS_FULFILLED,
 } from '../actions/actionTypes';
 
@@ -19,54 +17,6 @@ function projectsReducer(state = initialState, action) {
     case FETCH_ALL_PROJECTS_FULFILLED:
       return {
         ...action.payload.projects,
-      };
-
-    case DOWNLOAD_COMPLETED:
-      return {
-        ...state,
-        [action.payload.project.packageName]: projectReducer(state[action.payload.project.packageName], action),
-      };
-
-    default:
-      return state;
-  }
-}
-
-/**
- * Reducer for a single project.
- *
- * @param {Object} state
- * @param {Object} action
- * @returns {Object}
- */
-function projectReducer(state, action) {
-  switch(action.type) {
-
-    case DOWNLOAD_STARTED:
-      return {
-        ...state,
-        uploads: {
-          ...state.uploads,
-          [action.payload.timestamp]: {
-            ...state.uploads[action.payload.timestamp],
-            downloading: true,
-          },
-        },
-      };
-
-
-    case DOWNLOAD_COMPLETED:
-      return {
-        ...state,
-        uploads: {
-          ...state.uploads,
-          [action.payload.timestamp]: {
-            ...state.uploads[action.payload.timestamp],
-            downloading: false,
-            downloaded: true,
-            apkPath: action.payload.apkPath,
-          },
-        },
       };
 
     default:
